32aa3847d01ec79e01ba40faddb133cbea5bbd75
cito711 <garciape711github@hotmail.com>
2026-03-06 14:21:23 -0600

    adding gitignore file and initial c++ file

---
+++ GradeBookHT/GradeBookHT.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int getGrades() {
+
+}
+
+int findLowest() {
+
+}
+
+int findAverage() {
+
+}
+
+int calculateWeighted() {
+
+}
+
+int writeToFile() {
+
+}
+
+int main() {
+
+}
